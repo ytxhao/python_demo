3028ca09d78b70b2d056c81bd4d133dd83ac55cc
hao.yu <hao.yu@ushow.media>
2022-04-24 15:44:05 +0800

    更新解压文件显示进度

--- pythonProject/demo2/demo2.py
+++ pythonProject/demo2/demo2.py
@@ -11,6 +11,8 @@ if sys.version > '3':
 else:
     import Queue
 import commands
+import operator
+import copy
 import os
 import sys
 import tarfile
@@ -40,9 +42,15 @@ CMAKE_BUILD_SCRIPT = os.path.normpath(os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23
 def run_test(q, tarf):
     print("run_test===")
     while not q.empty():
-        name_ = q.get()
+        tarinfo = q.get()
         try:
-            tarf.extract(name_, path=os.path.join(CMAKE_ROOT_DIR, 'source'))
+            # tarf.extract(name_, path=os.path.join(CMAKE_SOURCE_DIR))
+            if tarinfo.isdir():
+                # Extract directories with a safe mode.
+                # directories.append(tarinfo)
+                tarinfo = copy.copy(tarinfo)
+                tarinfo.mode = 0700
+            tarf.extract(tarinfo, CMAKE_SOURCE_DIR)
         except Exception as e:
             pass
         q.task_done()  # 处理完成
@@ -68,6 +76,19 @@ def show_test(q, q_len):
     progressbar(q_len, q_len)
 
 
+def show_test2(count, len):
+    # num = count.cnt()
+    # print("show_test2===num:", num)
+    while True:
+        num = count.cnt()
+        if num != 0:
+            time.sleep(0.5)
+            progressbar(len - num, len)
+        else:
+            break
+    progressbar(len, len)
+
+
 def mult_thread_test():
     shutil.rmtree(os.path.normpath(os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1')), True)
     cmake_tar_file = os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1.tar.gz')
@@ -75,16 +96,22 @@ def mult_thread_test():
     q_len = 0  # 记录队列长度
     tarf = tarfile.open(cmake_tar_file)
     names = tarf.getnames()
+    numbers = tarf.getmembers()
     threadlist = []
-    for name in names:
+    for name in numbers:
         q.put(name)
         q_len += 1
     # def __init__(self, group=None, target=None, name=None,
     #              args=(), kwargs=None, verbose=None):
     # 处理线程
-    for x in range(0, 2):
-        th = threading.Thread(target=run_test, args=(q, tarf,))
-        threadlist.append(th)
+    # lock = threading.Lock()
+    # for x in range(0, 8):
+    #     th = threading.Thread(target=run_test, args=(q, tarf,))
+    #     th.setDaemon(True)
+    #     threadlist.append(th)
+    th = threading.Thread(target=extract_test, args=(q, tarf,))
+    th.setDaemon(True)
+    threadlist.append(th)
 
     # 进度线程
     threadlist.append(threading.Thread(target=show_test, args=(q, q_len,)))
@@ -108,12 +135,128 @@ def single_thread():
     tar.close()
 
 
-if __name__ == '__main__':
+def extract_test(q, q_len):
+    shutil.rmtree(os.path.normpath(os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1')), True)
+    cmake_tar_file = os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1.tar.gz')
+    tar = tarfile.open(cmake_tar_file)
 
+    directories = []
+
+    # if members is None:
+    members = tar.getmembers()
+    path = CMAKE_SOURCE_DIR
+    for tarinfo in members:
+        if tarinfo.isdir():
+            # Extract directories with a safe mode.
+            directories.append(tarinfo)
+            tarinfo = copy.copy(tarinfo)
+            tarinfo.mode = 0700
+        tar.extract(tarinfo, path)
+        # q.task_done()  # 处理完成
+
+    # Reverse sort directories.
+    directories.sort(key=operator.attrgetter('name'))
+    directories.reverse()
+
+    # Set correct owner, mtime and filemode on directories.
+    # for tarinfo in directories:
+    #     dirpath = os.path.join(path, tarinfo.name)
+    #     try:
+    #         self.chown(tarinfo, dirpath)
+    #         self.utime(tarinfo, dirpath)
+    #         self.chmod(tarinfo, dirpath)
+    #     except ExtractError, e:
+    #         if self.errorlevel > 1:
+    #             raise
+    #         else:
+    #             self._dbg(1, "tarfile: %s" % e)
+
+
+def extract_test3():
+    shutil.rmtree(os.path.normpath(os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1')), True)
+    cmake_tar_file = os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1.tar.gz')
+    tar = tarfile.open(cmake_tar_file)
+    tar.extractall(CMAKE_SOURCE_DIR)
+    tar.close()
+
+
+def extract_test2(count):
+    shutil.rmtree(os.path.normpath(os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1')), True)
+    cmake_tar_file = os.path.join(CMAKE_SOURCE_DIR, 'cmake-3.23.1.tar.gz')
+    tar = tarfile.open(cmake_tar_file)
+
+    directories = []
+
+    # if members is None:
+    members = tar.getmembers()
+    # members.count()
+    count.set(len(members))
+
+    th = threading.Thread(target=show_test2, args=(count, len(members),))
+    th.start()
+
+    path = CMAKE_SOURCE_DIR
+    for tarinfo in members:
+        if tarinfo.isdir():
+            # Extract directories with a safe mode.
+            directories.append(tarinfo)
+            tarinfo = copy.copy(tarinfo)
+            tarinfo.mode = 0700
+        tar.extract(tarinfo, path)
+        count.sub()
+
+
+    # Reverse sort directories.
+    directories.sort(key=operator.attrgetter('name'))
+    directories.reverse()
+
+    # Set correct owner, mtime and filemode on directories.
+    # for tarinfo in directories:
+    #     dirpath = os.path.join(path, tarinfo.name)
+    #     try:
+    #         self.chown(tarinfo, dirpath)
+    #         self.utime(tarinfo, dirpath)
+    #         self.chmod(tarinfo, dirpath)
+    #     except ExtractError, e:
+    #         if self.errorlevel > 1:
+    #             raise
+    #         else:
+    #             self._dbg(1, "tarfile: %s" % e)
+    th.join()
+
+
+class Count:
+
+    def __init__(self):
+        self.num = 0
+        # self.lock = threading.Lock()
+
+    def add(self):
+        # with self.lock:
+        self.num = self.num + 1
+
+    def sub(self):
+        # with self.lock:
+        self.num = self.num - 1
+
+    def set(self, cnt):
+        # with self.lock:
+        self.num = cnt
+
+    def cnt(self):
+        # with self.lock:
+        return self.num
+
+
+if __name__ == '__main__':
     start_sec = time.time()
     print("==============CMAKE_SOURCE_DIR:" + CMAKE_SOURCE_DIR)
-
-    single_thread()
+    # extract_test2()
+    # extract_test()
+    count = Count()
+    extract_test2(count)
+    # single_thread()
+    # mult_thread_test()
     end_sec = time.time()
     consume_sec = end_sec - start_sec
     # print("==============main end")
